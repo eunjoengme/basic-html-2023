91d788c1dca566ebee7510cd2a538f4a60489739
eunjoengme <dbe05126@gmail.com>
2023-04-04 17:03:21 +0900

    수정

--- Day11/slideshow.js
+++ Day11/slideshow.js
@@ -26,3 +26,19 @@ function nextSlide() {
     else current = 0;
     showSlides(current);
 }
+
+// 이미지 슬라이드 쇼를 자동으로 바꾸기
+// var current = 0;
+// showSlides();
+
+// function showSlides(){
+//     var slides = document.querySelectorAll("#slides > img");
+//     for(let i =0; i < slides.length; i++){
+//         slides[i].style.display = "none";
+//     }
+//     current++;
+//     if(current > slides.length)
+//     current =1;
+//     slides[current - 1].style.display = "block";
+//     setTimeout(showSlides, 2000);
+// }
